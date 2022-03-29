1aa67f286f53375120d6277c99f954aaa99071d3
Luis Suarez <lsuarezh23@gmail.com>
2022-03-29 18:03:05 -0400

    prod files

---
+++ www/6492.ceb2650e7430d533.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[6492],{6492:(h,l,s)=>{s.r(l),s.d(l,{ion_text:()=>n});var o=s(1024),a=s(6774),c=s(7506);let n=class{constructor(t){(0,o.r)(this,t)}render(){const t=(0,a.b)(this);return(0,o.h)(o.H,{class:(0,c.c)(this.color,{[t]:!0})},(0,o.h)("slot",null))}};n.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
